d9ea9e2b2fca162af454bcebc7b2f807e50bf294
accurate760 <hafizmuhammadahmadamin@gmail.com>
2026-05-07 10:32:49 +0500

    Done

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Iag8f6XWC0WoSVfMICvBBchZ4qHzGak7qNKCtvk2HxA=\"\n}"
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"K0RvzRwE86l4crlx3GZLWL4cMEBBel1M6BdSApjq/KI=\"\n}"
+>>>>>>> 8354fb84629d3833d0b42899a0b7cbd2082929c6
